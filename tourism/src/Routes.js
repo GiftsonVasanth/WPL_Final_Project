729648e28b1424d7922a658d15fab9f3f0ae42ca
kkbacktracks <kaustubhrk10@gmail.com>
2021-11-16 15:04:14 -0600

    Initial Commit

---
+++ tourism/src/Routes.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import {useState} from 'react';
+import HomePage from './HomePage';
+import SignIn from './modules/views/SignIn';
+import SignUp from './modules/views/SignUp';
+import withRoot from './modules/withRoot';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function Index() {
+  const [ user, setLoginUser] = useState({})
+
+  return (
+    <Router>
+      <Routes>
+      <Route path="/" element={<HomePage data={user}/>} />
+        <Route path="/signIn" element={<SignIn setLoginUser={setLoginUser} />} />
+        <Route path="/register" element={<SignUp setLoginUser={setLoginUser} />} />
+
+      </Routes>
+    </Router>
+    
+    
+  );
+}
+
+export default withRoot(Index);
