dd849c3489ee7529d4a155845c7481982e744048
kkbacktracks <kaustubhrk10@gmail.com>
2021-11-21 11:54:16 -0600

    Adding Pagination

--- Frontend/src/Items.js
+++ Frontend/src/Items.js
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppAppBar from './modules/views/AppAppBar';
 import AppFooter from './modules/views/AppFooter';
+import { Pagination } from "@material-ui/lab";
+import usePagination from "./pagination";
 
  
 
@@ -21,19 +23,32 @@ export default function Dynamic() {
     const classes = useStyles();
     const [attractionData, setAttractionData] = useState([]);
 
+    let [page, setPage] = useState(1);
+    const PER_PAGE = 24;
+
+    
+
     useEffect(() => {
         const fetchData = async () => {
             const response = await fetch('http://localhost:9002/api/items')
             const attractionsData = await response.json()
-            setAttractionData(attractionsData.slice(0, 15))
+            setAttractionData(attractionsData.slice(0, 100))
         }
         fetchData()
     }, [])
 
+    const count = Math.ceil(attractionData.length / PER_PAGE);
+    const _DATA = usePagination(attractionData, PER_PAGE);
+
+    const handleChange = (e, p) => {
+        setPage(p);
+        _DATA.jump(p);
+    };
+
     return (
         <><AppAppBar /><Container>
             <Row>
-                {attractionData.map((attractionData, k) => (
+                {_DATA.currentData().map((attractionData, k) => (
                     <Col key={k} xs={12} md={4} lg={3}>
                         <Card className={classes.root}>
                             <Card.Img variant="top" src="https://fastly.4sqi.net/img/general/200x200/7990081_d_2diKN7oPHt_6Eq-pjt3Fh7oWMwiRD3Ciu5hw-OZfk.jpg" />
@@ -48,6 +63,14 @@ export default function Dynamic() {
                     </Col>
                 ))}
             </Row>
+            <Pagination
+                count={count}
+                size="large"
+                page={page}
+                variant="outlined"
+                shape="rounded"
+                onChange={handleChange}
+            />
         </Container><AppFooter /></>
     )
 }
